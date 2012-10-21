7b8bb90f37db244dcfd2373342d883dc1546e2ef
rod <rod@pu-gh.com>
2012-10-21 10:47:49 +0100

    tweaked styling

--- src/lewis/layout.clj
+++ src/lewis/layout.clj
@@ -6,23 +6,26 @@
   (html
     [:head
       [:title (format "Lewis - %s" title)]
-      (include-css "/assets/bootstrap/css/bootstrap.css")]
+      (include-css "/assets/bootstrap/css/bootstrap.css")
+      (include-css "/assets/css/main.css")]
     [:body
       [:div.container
-        [:div.row
-          [:div.span12
-            [:h1 "Lewis"]
-            [:ul
-              [:li
-                [:a {:href "/"} "Connect"]]
-              [:li
-                [:a {:href "/session"} "Home"]]
-              [:li
-                [:a {:href "/session/schema"} "Schema"]]
-              [:li
-                [:a {:href "/session/query"} "Query"]]
-              [:li
-                [:a {:href "/session/transact"} "Transact"]]]]]
+        [:div.navbar.navbar-inverse.navbar-fixed-top
+          [:div.navbar-inner
+            [:div.container
+              [:a.brand {:href "/"} "Lewis"]
+              [:ul.nav
+                [:li
+                  [:a {:href "/"} "Connect"]]
+                [:li
+                  [:a {:href "/session"} "Home"]]
+                [:li
+                  [:a {:href "/session/schema"} "Schema"]]
+                [:li
+                  [:a {:href "/session/query"} "Query"]]
+                [:li
+                  [:a {:href "/session/transact"} "Transact"]]]
+              ]]]
         content
         [:div.row
           [:div.span12.footer
